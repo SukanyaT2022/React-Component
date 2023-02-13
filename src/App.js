6563e44677f226a48a75dbb0de8bffe66428c8a9
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-13 10:07:18 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <Box>
+<Box bg='tomato'>Home</Box>
+<Box bg='green'>middle</Box>
+<Box bg='blue'>right</Box>
+
+    </Box>
+  )
+}
+
+export default App
